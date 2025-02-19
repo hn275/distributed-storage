51cb94f1b64649f16b5ca744324d260f3bfe89b6
Hal <haln_01@proton.me>
2025-02-19 10:11:00 -0800

    datanode - lb initial connection

--- cmd/lb/main.go
+++ cmd/lb/main.go
@@ -7,6 +7,8 @@ import (
 	"log/slog"
 	"net"
 	"os"
+
+	"github.com/hn275/distributed-storage/internal/network"
 )
 
 var (
@@ -20,7 +22,7 @@ func init() {
 }
 
 func main() {
-	soc, err := net.Listen("tcp", serverAddr)
+	soc, err := net.Listen(network.ProtoTcp4, serverAddr)
 	if err != nil {
 		logger.Error("failed to start node", "err", err)
 		// TODO: define a set of error codes and semantics to share between binaries
@@ -35,40 +37,28 @@ func main() {
 		"address", soc.Addr(),
 	)
 
+	// array buffer to hold all the data nodes in the cluster
+	// TODO: Emily can define this data structure later
+	dataNodes := make(map[net.Addr]net.Conn)
+	go func() {
+		for _, nodeConn := range dataNodes {
+			closeConn(nodeConn)
+		}
+	}()
+
+	var buf [256]byte
 	for {
 		conn, err := soc.Accept()
 		if err != nil {
-			logger.Error("failed to except new connection.",
+			logger.Error("failed to accept new conn.",
 				"peer", conn.RemoteAddr,
 				"err", err,
 			)
 			continue
 		}
 
-		go serviceConn(conn)
-	}
-}
-
-func serviceConn(conn net.Conn) {
-	// disconnect
-	defer func() {
-		if err := conn.Close(); err != nil && !errors.Is(err, io.EOF) {
-			logger.Error("failed to close connection",
-				"peer", conn.RemoteAddr(),
-				"err", err,
-			)
-			return
-		}
-
-		logger.Info("connection closed", "peer", conn.RemoteAddr())
-	}()
-
-	logger.Info("servicing connection.", "peer", conn.RemoteAddr())
-	buf := make([]byte, 1024)
-
-	for {
-		n, err := conn.Read(buf)
-		if err != nil {
+		// handling the initial ping
+		if _, err = conn.Read(buf[:]); err != nil {
 			if errors.Is(err, io.EOF) {
 				// peer disconnected, silent return
 				return
@@ -81,7 +71,32 @@ func serviceConn(conn net.Conn) {
 			return
 		}
 
-		buf = buf[:n]
-		logger.Info("message received.", "peer", conn.RemoteAddr(), "msg", string(buf))
+		logger.Info("new connection.", "peer", conn.RemoteAddr())
+		switch buf[0] {
+		case network.DataNodeJoin:
+			dataNodes[conn.RemoteAddr()] = conn
+			go serveDataNode(conn)
+			logger.Info("data node joined cluster.", "addr", conn.RemoteAddr())
+
+		default:
+			logger.Error("unsupported ping message type.", "msgtype", buf[0])
+			closeConn(conn)
+		}
 	}
+}
+
+func serveDataNode(conn net.Conn) {
+	// TODO: need a way to remove this connection from the cluster map
+	// when the node leaves the cluster/code exploded
+}
+
+func closeConn(conn net.Conn) {
+	if err := conn.Close(); err != nil && !errors.Is(err, io.EOF) {
+		logger.Error("failed to close connection",
+			"peer", conn.RemoteAddr(),
+			"err", err,
+		)
+	}
+
+	logger.Info("connection closed", "peer", conn.RemoteAddr())
 }
