76425756442f2e3566c5703ff418952bd8ab191b
Hal <haln_01@proton.me>
2025-02-27 00:53:53 -0800

    added tests for addr - byte conversion

--- internal/network/network_test.go
+++ internal/network/network_test.go
@@ -1,1 +1,24 @@
 package network
+
+import (
+	"encoding/binary"
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddrByteConv(t *testing.T) {
+	addr := net.TCPAddr{
+		IP:   []byte{192, 168, 1, 1},
+		Port: 0xff00,
+	}
+
+	var buf [6]byte
+	assert.Nil(t, AddrToBytes(&addr, buf[:]))
+	assert.Equal(t, addr.IP[0], buf[0])
+	assert.Equal(t, addr.IP[1], buf[1])
+	assert.Equal(t, addr.IP[2], buf[2])
+	assert.Equal(t, addr.IP[3], buf[3])
+	assert.Equal(t, uint16(addr.Port), binary.LittleEndian.Uint16(buf[4:6]))
+}
