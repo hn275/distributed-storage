405c0b11a54ebd16ad5b565f12c78b6100a18d7f
Hal <haln_01@proton.me>
2025-03-01 17:59:53 -0800

    added encryption functions

--- go.mod
+++ go.mod
@@ -12,7 +12,11 @@ require (
 
 require (
 	github.com/dustin/go-humanize v1.0.1
+	golang.org/x/crypto v0.35.0
 	lukechampine.com/blake3 v1.3.0
 )
 
-require github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+require (
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+)
---
+++ internal/crypto/crypto_test.go
@@ -0,0 +1,68 @@
+package crypto
+
+import (
+	"crypto/rand"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncrypt(t *testing.T) {
+	plaintext := "Hello, world!"
+
+	buf := make([]byte, NonceSize+OverHead+len(plaintext))
+
+	_, err := io.ReadFull(rand.Reader, buf[:NonceSize])
+	assert.Nil(t, err)
+
+	copy(buf[NonceSize:], []byte(plaintext))
+
+	tmp := make([]byte, len(buf))
+	copy(tmp, buf)
+
+	err = Encrypt(
+		buf[NonceSize:NonceSize],
+		DataNodeSecretKey[:],
+		buf[:NonceSize],
+		buf[NonceSize:NonceSize+len(plaintext)],
+		UserPublicKey[:],
+	)
+	assert.Nil(t, err)
+
+	for i := 0; i < NonceSize; i++ {
+		assert.Equal(t, tmp[i], buf[i])
+	}
+
+	for i := NonceSize; i < len(buf)-NonceSize; i++ {
+		assert.NotEqual(t, tmp[i], buf[i])
+	}
+}
+
+func TestDecrypt(t *testing.T) {
+	// encrypt data
+	plaintext := "Hello, world!"
+	buf := make([]byte, NonceSize+OverHead+len(plaintext))
+	_, _ = io.ReadFull(rand.Reader, buf[:NonceSize])
+	copy(buf[NonceSize:], []byte(plaintext))
+
+	_ = Encrypt(
+		buf[NonceSize:NonceSize],
+		DataNodeSecretKey[:],
+		buf[:NonceSize],
+		buf[NonceSize:NonceSize+len(plaintext)],
+		UserPublicKey[:],
+	)
+
+	// decrypt data
+	err := Decrypt(
+		buf[:0],
+		DataNodeSecretKey[:],
+		buf[:NonceSize],
+		buf[NonceSize:],
+		UserPublicKey[:],
+	)
+
+	assert.Nil(t, err)
+	assert.Equal(t, plaintext, string(buf[:len(plaintext)]))
+}
