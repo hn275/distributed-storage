6209065acfc2cf1cabdb500b61a2344c33bb01d1
Hal <haln_01@proton.me>
2025-03-10 15:21:12 -0700

    implemented least response time

--- internal/algo/lrt.go
+++ internal/algo/lrt.go
@@ -1,42 +1,39 @@
 package algo
 
 import (
+	"container/heap"
+	"errors"
 	"math"
 	"net"
 )
 
-type LRTNode struct {
-	net.Conn
-	avgResponseTime float64
+type LeastResponseTime struct {
+	priorityQueue
 }
 
-type LeastResponseTime []*LRTNode
-
-// LeastResponseTime implements sort.Interface
-func (lrt LeastResponseTime) Len() int {
-	return len(lrt)
+// LeastResponseTime implements LBAlgo
+func (lrt *LeastResponseTime) Initialize() {
+	// nop
 }
 
-// LeastResponseTime implements sort.Interface. This math assume NaN to be the
-// smallest value, see https://pkg.go.dev/sort#Float64Slice.Less
-func (lrt LeastResponseTime) Less(i, j int) bool {
-	return (lrt[i].avgResponseTime < lrt[j].avgResponseTime) ||
-		(math.IsNaN(lrt[i].avgResponseTime) && !math.IsNaN(lrt[j].avgResponseTime))
+// LeastResponseTime implements LBAlgo
+func (lrt *LeastResponseTime) NodeJoin(conn net.Conn) error {
+	heap.Push(lrt, queueNode{conn, 0.0})
+	return nil
 }
 
-// LeastResponseTime implements sort.Interface
-func (lrt LeastResponseTime) Swap(i, j int) {
-	lrt[i], lrt[j] = lrt[j], lrt[i]
-}
+// LeastResponseTime implements LBAlgo
+func (lrt *LeastResponseTime) GetNode() (net.Conn, error) {
+	if lrt.Len() == 0 {
+		return nil, errors.New("no node to dispatch")
+	}
 
-// LeastResponseTime implements heap.Interface
-// since nodes aren't joining/leaving, no need to implement
-func (lrt LeastResponseTime) Push(x any) {
-	panic("not implemented")
-}
+	node := lrt.priorityQueue[0].Conn
 
-// LeastResponseTime implements heap.Interface
-// since nodes aren't joining/leaving, no need to implement
-func (lrt LeastResponseTime) Pop() any {
-	panic("not implemented")
+	if lrt.priorityQueue[0].weight == 0 {
+		lrt.priorityQueue[0].weight = math.MaxFloat64
+		heap.Init(lrt)
+	}
+
+	return node, nil
 }
