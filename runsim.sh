093ffb18dba92f82d846bfd9ab2730f43a857349
Hal <haln_01@proton.me>
2025-03-23 17:23:28 -0700

    run simulation script

---
+++ runsim.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+
+mkdir -p log/lb/
+mkdir -p log/cluster/
+mkdir -p log/user/
+
+runsim() {
+	lblogfile="log/lb/lb-$(basename "$1" .yaml).log"
+	clusterlogfile="log/cluster/cluster-$(basename "$1" .yaml).log"
+	userlogfile="log/user/user-$(basename "$1" .yaml).log"
+
+	export CONFIG_PATH=$1
+	nohup go run ./cmd/loadbalance >$lblogfile 2>&1 &
+	nohup go run ./cmd/cluster >$clusterlogfile 2>&1 &
+	go run ./cmd/user >$userlogfile 2>&1
+}
+
+for file in ./config/*; do
+	if [ -f "$file" ]; then
+		echo "Running simulation: $file"
+		runsim $file
+	fi
+done
