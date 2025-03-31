e799e8f67f9775fe8c762fe3ad43b4b0823307fb
Hal <haln_01@proton.me>
2025-03-30 18:12:15 -0700

    updated sim run script

--- runsim.sh
+++ runsim.sh
@@ -1,21 +1,34 @@
 #!/usr/bin/bash
 
+echo "Removing existing output"
+rm -fr ./tmp/output
+
+echo "Creating log dir"
 mkdir -p log/lb/
 mkdir -p log/cluster/
 mkdir -p log/user/
+
+echo "Compiling binaries"
+go build -ldflags='-s -w' -o tmp/cluster ./cmd/cluster
+go build -ldflags='-s -w' -o tmp/loadbalance ./cmd/loadbalance
+go build -ldflags='-s -w' -o tmp/user ./cmd/user
 
 runsim() {
 	lblogfile="log/lb/lb-$(basename "$1" .yaml).log"
 	clusterlogfile="log/cluster/cluster-$(basename "$1" .yaml).log"
 	userlogfile="log/user/user-$(basename "$1" .yaml).log"
 
+	# kill the process binding port 8000 (if exists)
+	pid=$(lsof -t -i:8000)
+	[[ -z $pid ]] || kill -9 $(lsof -t -i:8000)
+
 	export CONFIG_PATH=$1
+	sleep 3
+	./tmp/loadbalance &
 	sleep 1
-	nohup go run ./cmd/loadbalance >$lblogfile 2>&1 &
+	./tmp/cluster &
 	sleep 1
-	nohup go run ./cmd/cluster >$clusterlogfile 2>&1 &
-	sleep 1
-	go run ./cmd/user
+	./tmp/user
 }
 
 for file in ./config/*; do
